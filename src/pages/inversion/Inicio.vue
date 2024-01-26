358d339f643d7b73b7d9d002dccaf6f9f8bf368e
Fredy Rojas <farojas851@gmail.com>
2024-01-25 19:26:48 -0500

    feat: Vista registro Inversiones

--- src/pages/inversion/Inicio.vue
+++ src/pages/inversion/Inicio.vue
@@ -1,2 +1,57 @@
-<template></template>
-<script setup></script>
+<script setup>
+import { onMounted, ref, inject } from 'vue';
+import { defineTitle } from '../../helpers';
+import ContentHeader from '../../components/ContentHeader.vue';
+import { useDatosSession } from '../../composables/session';
+
+const { usuario, puede } = useDatosSession();
+
+const Auth = inject('Auth');
+
+const titleHeader = ref({
+    titulo:'Historial Inversión',
+    icon:'fas fa-file-invoice-dollar',
+    subTitulo:'Registro Inversiones',
+    vista:"Historial Inversión"
+})
+
+
+onMounted(() => {
+    defineTitle(titleHeader.value.vista);
+    cambiarVista('Historial Inversión','fas fa-file-invoice-dollar');
+})
+
+const cambiarVista =(nuevaVista, icono) => {
+    titleHeader.value.vista = nuevaVista;
+    titleHeader.value.titulo = nuevaVista;
+    titleHeader.value.subTitulo = nuevaVista;
+    titleHeader.value.icon = icono
+    defineTitle(titleHeader.value.vista)
+}
+
+</script>
+<template>
+    <ContentHeader :titleHeader="titleHeader"></ContentHeader>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <button class="btn btn-app bg-primary"
+                                @click.prevent="cambiarVista('Historial Inversión','fas fa-file-invoice-dollar')"
+                                v-if="puede('registro-inversiones.historial')">
+                                <i class="fas fa-file-invoice-dollar"></i> Historial Inversiones
+                            </button>
+                            <button class="btn btn-app bg-primary"
+                                @click.prevent="cambiarVista('Registro Inversión','fas fa-hand-holding-dollar')"
+                                v-if="puede('registro-inversiones.nuevo')">
+                                <i class="fas fa-hand-holding-dollar"></i> Registrar Inversiones
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
